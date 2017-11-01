6272817b7e90f81eb3f9ece912db0b88ccadb0ce
yzyDavid <yzyDavid@qq.com>
2017-11-02 03:00:22 +0800

    added a dummy ThemeAlgorithm.

--- src/app/config.py
+++ src/app/config.py
@@ -1,1 +1,2 @@
 DEBUG = True
+port = 5000
---
+++ src/app/theme_algorithm.py
@@ -0,0 +1,12 @@
+from sklearn.cluster import KMeans
+from .algorithm import Algorithm
+
+
+# noinspection PyCompatibility
+class ThemeAlgorithm(Algorithm):
+    def __init__(self):
+        super().__init__()
+        n_colors = 5
+
+    def run(self, *args, **kwargs):
+        pass
