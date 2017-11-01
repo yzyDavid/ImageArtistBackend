c7865f172014c9712848387e56079d446e1a8c1a
yzyDavid <yzyDavid@qq.com>
2017-11-02 02:44:53 +0800

    refine dir structure.

--- src/app/app.py
+++ src/app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
+from .config import DEBUG
+
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def hello():
     return 'ImageArtist Backend API'
 
