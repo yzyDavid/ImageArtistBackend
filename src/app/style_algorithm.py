91484eb220b374a541f3c0e6d7501d53addcf76b
yzyDavid <yzyDavid@qq.com>
2017-11-04 23:55:45 +0800

    initialized StyleAlgorithm class.

---
+++ src/app/style_algorithm.py
@@ -0,0 +1,13 @@
+from .algorithm import Algorithm
+
+
+# noinspection PyCompatibility
+class StyleAlgorithm(Algorithm):
+    def __init__(self):
+        super().__init__()
+
+    def serve(self, item: dict):
+        pass
+
+    def run(self, *args, **kwargs):
+        pass
